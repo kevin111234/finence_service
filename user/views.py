cf86d4555788426086f05c7ee2afcb960e56bb08
woghd111234 <jaehong243587@gmail.com>
2024-07-23 02:40:33 +0900

    로그인, 회원가입 화면 생성 및 뷰 수정

--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('user:profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'user/signup.html', {'form': form})
