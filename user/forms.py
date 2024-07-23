a1e777ee097e3fed0fdfea0c276967ede71ab910
woghd111234 <jaehong243587@gmail.com>
2024-07-23 12:03:03 +0900

    회원가입 및 로그인 시 표시되는 이름과 회원가입 시 요구사항 추가

--- user/forms.py
+++ user/forms.py
@@ -5,13 +5,35 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'date_of_birth', 'gender', 'password1', 'password2')
+        fields = ('last_name', 'first_name', 'username', 'email', 'date_of_birth', 'gender', 'password1', 'password2')
+        
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['email'].required = True
+        # 표시되는 이름 변경
+        self.fields['username'].label = '유저 ID'
+        self.fields['first_name'].label = '이름'
+        self.fields['last_name'].label = '성'
+        self.fields['email'].label = '이메일 주소'
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'date_of_birth', 'gender')
+    
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['email'].required = True
+        # 표시되는 이름 변경
+        self.fields['username'].label = '유저 ID'
+        self.fields['first_name'].label = '이름'
+        self.fields['last_name'].label = '성'
+        self.fields['email'].label = '이메일 주소'
 
 class CustomAuthenticationForm(AuthenticationForm):
-    username = forms.CharField(label='User ID', max_length=30, widget=forms.TextInput(attrs={'autofocus': True}))
+    username = forms.CharField(label='유저 ID', max_length=30, widget=forms.TextInput(attrs={'autofocus': True}))
     password = forms.CharField(label='Password', strip=False, widget=forms.PasswordInput)
