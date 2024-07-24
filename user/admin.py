1cee490ff2b258c51015049046864584c6b85d0e
woghd111234 <jaehong243587@gmail.com>
2024-07-24 19:43:35 +0900

    회원정보 수정 시 이름도 수정할 수 있도록 변경

--- user/admin.py
+++ user/admin.py
@@ -7,17 +7,17 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['username', 'email', 'gender', 'is_staff']
+    list_display = ['username', 'email', 'first_name', 'last_name', 'gender', 'is_staff']
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('email', 'gender')}),
+        ('Personal info', {'fields': ('email', 'first_name', 'last_name', 'gender')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'gender', 'password1', 'password2', 'is_active', 'is_staff'),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'gender', 'password1', 'password2', 'is_active', 'is_staff'),
         }),
     )
 
--- user/forms.py
+++ user/forms.py
@@ -6,7 +6,7 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('last_name', 'first_name', 'username', 'email', 'gender', 'password1', 'password2')
-        
+
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
         self.fields['first_name'].required = True
@@ -21,18 +21,14 @@ class CustomUserCreationForm(UserCreationForm):
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'gender')
-    
+        fields = ('username', 'email', 'first_name', 'last_name', 'gender')
+
     def __init__(self, *args, **kwargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['first_name'].required = True
-        self.fields['last_name'].required = True
-        self.fields['email'].required = True
-        # 표시되는 이름 변경
+        super(CustomUserChangeForm, self).__init__(*args, **kwargs)
         self.fields['username'].label = '유저 ID'
+        self.fields['email'].label = '이메일 주소'
         self.fields['first_name'].label = '이름'
         self.fields['last_name'].label = '성'
-        self.fields['email'].label = '이메일 주소'
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(label='유저 ID', max_length=30, widget=forms.TextInput(attrs={'autofocus': True}))
