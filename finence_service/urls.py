8f521db906aaf64dacc500da82684156d51aad3f
woghd111234 <jaehong243587@gmail.com>
2024-07-25 19:34:04 +0900

    url 연결 추가 및 views 연결 수정

--- finence_service/urls.py
+++ finence_service/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('user/', include('user.urls')),
-    path('exchange_rate/', include('exchange_rate.urls'))
+    path('exchange_rate/', include('a_exchange_rate.urls'))
 ]
