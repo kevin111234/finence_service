9edd9e166dc3ed12a34cad5a818ef0ee0a21716a
woghd111234 <jaehong243587@gmail.com>
2024-07-29 20:42:00 +0900

    크롤링 스케줄러 설정 변경 및 slack 알림 기능 추가 (함수 작성 필요)

--- finence_service/scheduler.py
+++ finence_service/scheduler.py
@@ -4,7 +4,7 @@ from django_apscheduler.jobstores import DjangoJobStore, register_events
 from django_apscheduler.models import DjangoJobExecution
 import logging
 from a_coin_analyze.tasks import crawling_coin
-from a_exchange_rate.tasks import crawling_exchange
+from a_exchange_rate.tasks import crawling_exchange, slack_exchange
 from a_stock_analyze.tasks import crawling_stock
 
 logger = logging.getLogger(__name__)
@@ -21,8 +21,16 @@ def start():
     except Exception as e:
         logger.error(f"Error adding job coin_crawling: {e}")
 
+    # 환율, 달러 인덱스 데이터 크롤링 (20분마다)
     try:
-        job = scheduler.add_job(crawling_exchange, CronTrigger(minute="*/30"), id="exchange_crawling", replace_existing=True)
+        job = scheduler.add_job(crawling_exchange, CronTrigger(minute="*/20"), id="exchange_crawling", replace_existing=True)
+        logger.info(f"Job {job.id} added to scheduler.")
+    except Exception as e:
+        logger.error(f"Error adding job exchange_crawling: {e}")
+
+    # Slack 환율정보 알림 (30분마다)
+    try:
+        job = scheduler.add_job(slack_exchange, CronTrigger(minute="*/30"), id="exchange_crawling", replace_existing=True)
         logger.info(f"Job {job.id} added to scheduler.")
     except Exception as e:
         logger.error(f"Error adding job exchange_crawling: {e}")
