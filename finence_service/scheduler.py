a0880903d20fe16fd4c299fc31d01d3a5052333d
woghd111234 <jaehong243587@gmail.com>
2024-07-30 13:14:11 +0900

    로그 출력을 print 문으로 수정

--- finence_service/scheduler.py
+++ finence_service/scheduler.py
@@ -7,8 +7,6 @@ from a_coin_analyze.tasks import crawling_coin
 from a_exchange_rate.tasks import crawling_exchange, slack_exchange
 from a_stock_analyze.tasks import crawling_stock
 
-logger = logging.getLogger(__name__)
-
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
@@ -17,44 +15,43 @@ def start():
 
     try:
         job = scheduler.add_job(crawling_coin, CronTrigger(minute="*/30"), id="coin_crawling", replace_existing=True)
-        logger.info(f"Job {job.id} added to scheduler.")
+        print(f"Job {job.id} added to scheduler.")
         print("coin 크롤링 작업 등록 완료")
     except Exception as e:
-        logger.error(f"Error adding job coin_crawling: {e}")
+        print(f"Error adding job coin_crawling: {e}")
 
     # 환율, 달러 인덱스 데이터 크롤링 (20분마다)
     try:
         job = scheduler.add_job(crawling_exchange, CronTrigger(minute="*/20"), id="exchange_crawling", replace_existing=True)
-        logger.info(f"Job {job.id} added to scheduler.")
+        print(f"Job {job.id} added to scheduler.")
         print("exchange rate 크롤링 작업 등록 완료")
     except Exception as e:
-        logger.error(f"Error adding job exchange_crawling: {e}")
+        print(f"Error adding job exchange_crawling: {e}")
 
     # Slack 환율정보 알림 (30분마다)
     try:
         job = scheduler.add_job(slack_exchange, CronTrigger(minute="*/30"), id="slack_notice", replace_existing=True)
-        logger.info(f"Job {job.id} added to scheduler.")
+        print(f"Job {job.id} added to scheduler.")
         print("slack 알림 작업 등록 완료")
     except Exception as e:
-        logger.error(f"Error adding job exchange_crawling: {e}")
+        print(f"Error adding job exchange_crawling: {e}")
 
     try:
         job = scheduler.add_job(crawling_stock, CronTrigger(minute="*/30"), id="stock_crawling", replace_existing=True)
-        logger.info(f"Job {job.id} added to scheduler.")
+        print(f"Job {job.id} added to scheduler.")
         print("stock 크롤링 작업 등록 완료")
     except Exception as e:
-        logger.error(f"Error adding job stock_crawling: {e}")
+        print(f"Error adding job stock_crawling: {e}")
 
     # 이벤트 등록
     register_events(scheduler)
 
     # 스케줄러 시작
     scheduler.start()
-    logger.info("Scheduler started...")
     print("스케줄러 실행 성공!")
 
     # 작업이 올바르게 등록되었는지 확인
     if job:
-        logger.info(f"Job {job.id} added to scheduler.")
+        print(f"Job {job.id} added to scheduler.")
     else:
-        logger.error("Job was not added to scheduler.")
+        print("Job was not added to scheduler.")
