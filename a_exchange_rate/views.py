af96c7cc368269434a417b77a65f7b8287943711
woghd111234 <jaehong243587@gmail.com>
2024-07-30 16:46:00 +0900

    환율정보 웹페이지에 게시

--- a_exchange_rate/views.py
+++ a_exchange_rate/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import redirect
+from .models import ExchangeRate
 
 def group_required(group_name):
     def in_group(user):
@@ -11,4 +12,6 @@ def group_required(group_name):
 
 @group_required('exchange_rate')
 def main(request):
-    return render(request, 'exchange_rate/main.html')
+    latest_rate = ExchangeRate.objects.order_by('-date').first()
+    # exchange_rates = ExchangeRate.objects.all().order_by('-date')  # 최신 데이터부터 정렬
+    return render(request, 'exchange_rate/main.html', {'latest_rate': latest_rate})
