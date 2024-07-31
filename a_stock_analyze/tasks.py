8ab696d5b7c5acf8fc361b2b9d58e1ce8bef9acb
woghd111234 <jaehong243587@gmail.com>
2024-08-01 01:50:58 +0900

    미국 주식 티커, 역사적 데이터 크롤링 작업 작성 후 migrate

--- a_stock_analyze/tasks.py
+++ a_stock_analyze/tasks.py
@@ -1,9 +1,27 @@
-def crawling_stock():
+import os
+import django
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finence_service.settings')
+django.setup()
+
+def stock_ticker_save():
     print("주식 데이터 크롤링 작업을 수행합니다.")
     # 주식 티커 업데이트
+    from a_stock_analyze.stock_crawling import ticker_update
+    ticker_update()
+
+def stock_data_save():
     # 주가정보 업데이트
+    from a_stock_analyze.stock_crawling import save_historical_stock_data
+    save_historical_stock_data()
 
 def crawling_statement():
     print("재무제표 크롤링 작업을 수행합니다.")
     # 분기별/ 월별로 수행
     # 재무제표 업데이트
+
+if __name__ == '__main__':
+    stock_ticker_save()
