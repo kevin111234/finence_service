5256b91d17e2c7030df773ec3e49dc3754e4b80b
woghd111234 <jaehong243587@gmail.com>
2024-08-04 16:19:52 +0900

    함수 실행 시 출력문 변경

--- a_stock_analyze/tasks.py
+++ a_stock_analyze/tasks.py
@@ -8,19 +8,19 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finence_service.settings')
 django.setup()
 
 def stock_ticker_save():
-    print("주식 티커 데이터 크롤링 작업을 수행합니다.")
+    print("주식 티커 데이터 작업을 수행합니다.")
     # 주식 티커 업데이트
     from a_stock_analyze.stock_crawling import ticker_update
     ticker_update()
 
 def stock_data_save():
-    print("주식 데이터 크롤링 작업을 수행합니다.")
+    print("주식 데이터 작업을 수행합니다.")
     # 주가정보 업데이트
     from a_stock_analyze.stock_crawling import save_historical_stock_data
     save_historical_stock_data()
 
 def crawling_statement():
-    print("재무제표 크롤링 작업을 수행합니다.")
+    print("재무제표 작업을 수행합니다.")
     # 분기별/ 월별로 수행
     # 재무제표 업데이트
 
